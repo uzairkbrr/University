6a7d6dd73b345f12e2d6be1c12807eece6b88945
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-04-08 14:13:46 +0500

    Add files via upload
    This is my first javascript program. In this program, I will simply print "Hello World" on the console.

---
+++ JavaScript/01 Hello world.js
@@ -0,0 +1,11 @@
+console.log("Hello world");
+console.log("This is my first program of javascript");
+console.log("Trust me its very easy");
+
+
+// ; is optional 
+// but its good programming practise to use semicolan ( ; ) after every single statement 
+
+// console.log() is used to display whatever you want to show in console 
+// if we want to print "Hello world" so we need to wrote "console.log("Hello world");"
+
