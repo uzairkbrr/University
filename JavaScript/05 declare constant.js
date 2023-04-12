476c110fa42332fc01315972322a2fe5d1ffcaa1
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-04-12 05:05:21 +0500

    Add files via upload
    What is a "const" data type in javascript?
    How can we use it?
    Explained in this code file with comments.

---
+++ JavaScript/05 declare constant.js
@@ -0,0 +1,16 @@
+// Author: Uzair Ahmad
+
+// declare constants
+
+//  for declaring constant in javascript we use "const" keyword;
+//  value of const will not be changed anywhere in program;
+
+const pi = 3.14;
+
+// The below line will through us an error 
+// we don't change value of const;
+// pi = 3.15 ;
+
+// We can do every operation with const but can't change its value;
+
+console.log(pi);
