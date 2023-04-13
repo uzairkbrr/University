90074b092d218578c404e831bb98b071b87093cd
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-04-13 05:03:37 +0500

    Add files via upload
    concept of string indexing in javascript

---
+++ JavaScript/06 string indexing.js
@@ -0,0 +1,25 @@
+// Author: Uzair Ahmad
+
+// string indexing
+
+let firstName = "Uzair";
+
+//  U  z  a  i  r ;
+//  0  1  2  3  4 ;
+
+// console.log(firstName[2]) ;  2nd index is of " a " so it will print " a " on console;
+
+// length of string 
+// firstName.length
+
+// this will print the lenght of firstName on console;
+console.log(firstName.length);
+
+// to access last index of string 
+// last index = length - 1 ;
+// because indexing start from 0;
+
+// if we dont know the length of our string and want to print the last index of string then the below line will help us;
+console.log(firstName.length[firstName.length-1]);
+
+// Note: spaces is also included in string 
