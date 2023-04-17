840e34f4aeac71b4074b9275c2b17950d03f0c47
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-04-17 05:03:04 +0500

    Add files via upload
    In JavaScript, undefined, null, and BigInt are special values that have specific meanings and uses.

---
+++ JavaScript/11 undefined,null and BigInt.js
@@ -0,0 +1,37 @@
+// Author: Uzair Ahmad
+
+// undefined 
+// null 
+// BigInt
+
+// 1. undefined:-
+// let firstName ;
+
+// console.log(typeof firstName);
+
+// 2. null:-
+
+// let myvar = null;
+// console.log(myvar);
+
+// console.log(typeof null);
+// when we want to check the typeof "null" then it will give us "object". This is bug/error in javascript; 
+
+// 3. BigInt:-
+
+// we can store maximum upto 9007199254740991 in number datatype;
+// if we want to store bigger number then this then we will be use BigInt datatype
+// we also use BigInt datatype for small number ;
+
+// there are two way to use BigInt;
+// 1:-
+// let myNumber1 = Bigint(8);
+// 2:-
+//  let myNumber2 = 123n;
+
+//  console.log(myNumber1 + myNumber2);
+
+// All operation are performed between BigInt datatypel
+// we didn't perform any operation between BigInt and other datatype's;  
+
+
