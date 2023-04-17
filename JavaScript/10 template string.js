e9d3597af576257878aab2edc8d2bdf250b4fa33
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-04-17 05:00:10 +0500

    Add files via upload
    template string is a new feature introduced in ECMAScript 6 (ES6) that allows for more flexible and concise string formatting in JavaScript. Template strings use backticks (`) instead of single or double quotes and allow for the interpolation of variables and expressions within a string.

---
+++ JavaScript/10 template string.js
@@ -0,0 +1,13 @@
+// Author: Uzair Ahmad
+
+// in javascript template string is a way in which we can include variable or expression in it.
+
+let age = 20;
+let fullname = "Uzair Ahmad";
+
+// My Name is Uzair Ahmad and my age is 22;
+// let aboutMe = "My Name is " + fullname + " and my age is " + age ;
+
+let aboutMe =  `My Name is ${fullname} and my age is ${age}`
+
+console.log(aboutMe);
