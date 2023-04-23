7731f5a831c8607603baf75f5107d93eb052d5e9
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-04-23 13:52:14 +0500

    Add files via upload

---
+++ JavaScript/17 if else ladder.js
@@ -0,0 +1,19 @@
+// Author: Uzair Ahmad
+
+// if else ladder :
+
+let tempInDegree = 15;
+
+if(tempInDegree < 0){
+    console.log("Temp very cold outside!");
+}else if(tempInDegree < 16){
+    console.log("It is cold outside!");
+}else if(tempInDegree < 25){
+    console.log("wheather is Okay!");
+}else if(tempInDegree < 35){
+    console.log("Let's go for swim!");
+}else if(tempInDegree < 45){
+    console.log("Turn on AC!");
+}else{
+    console.log("Too Hot!");
+}
