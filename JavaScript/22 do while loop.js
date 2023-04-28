32581fbd971d72c20d8384131632559e52a8888a
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-04-29 00:43:03 +0500

    Add files via upload

---
+++ JavaScript/22 do while loop.js
@@ -0,0 +1,20 @@
+// Author: Uzair Ahmad
+
+// do while loop : 
+// do-while loop is similar to a while loop, except that the code block is executed at least once, regardless of whether the condition is true or false.
+
+// do-while loop will first execute the code once which is inside do;
+// then check the condition;
+
+let i=20;
+
+do{
+    console.log(i);
+    i++;
+}while(i<=10);
+
+// Output: 20
+
+// output is 20 because value of i is 20 so code inside do will execute once then check the condition if condition is true then it will execute again and so on 
+// and if condition is false then it will stop executing ;
+// main point of do-while loop is that it must be execute once no matter condition is true or false;
