e2d2f7e424568ac34880327e12a31394e6e36a50
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-05-16 00:57:52 +0500

    Add files via upload

---
+++ JavaScript/32 for in loop.js
@@ -0,0 +1,9 @@
+// Author: Uzair Ahmad 
+
+const fruits = ["apple", "banana", "grapes", "mango"];
+
+// for in loop gives us index number 
+// fruit is index of fruits array in the below code
+for(let fruit in fruits){
+   console.log(fruits[fruit]);
+}
