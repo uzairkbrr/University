d94377f24e93a1461c0ecb18ec510086920ba42b
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-05-17 02:53:54 +0500

    Add files via upload

---
+++ JavaScript/37 Computed properties.js
@@ -0,0 +1,20 @@
+// Author: Uzair Ahmad 
+
+const key1 = "objkey1";
+const key2 = "objkey2";
+
+const value1 = "myvalue1";
+const value2 = "myvalue2";
+
+// create an object and assign key1 and key2 and its member and value1,value2 as values of key1 and key2;
+
+
+const object = {
+    [key1] : value1,
+    [key2] : value2
+};
+
+// object[key1] = value1;
+// object[key2] = value2;
+
+console.log(object); 
