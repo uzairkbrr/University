0d6bf51dc1278fcf119f13998dd03f7cbbdefe4b
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-05-23 18:07:39 +0500

    Add files via upload

---
+++ JavaScript/49 rest parameter.js
@@ -0,0 +1,11 @@
+// Author: Uzair Ahmad
+
+//first two values will be stored in a and b 
+// rest of the values will be stored in c in the form of array
+function myFunc(a,b,...c){
+    console.log(`a is ${a}`);
+    console.log(`a is ${b}`);
+    console.log(`a is `,c);
+}
+
+myFunc(1,2,3,4,5,6,7);
