9ca9a2c9c82590c2a8ebb1e4d27a8a4354a28cbc
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-06-01 04:32:17 +0500

    Add files via upload

---
+++ C Programming/Loop/do while/Takes two odd numbers as input from the user and calculates their sum .c
@@ -0,0 +1,38 @@
+// Author: Uzair Ahmad
+#include <stdio.h>
+
+int main() {
+    
+   int n1, n2;
+
+	printf("Enter 1st Odd Number => "); // Prompt user to enter first odd number
+	
+	do{
+	    scanf("%d", &n1); // Take input from user
+	    
+	    if(n1 % 2 == 0) // Check if the number is even
+	    {
+	        printf("Number 1 is Even !\n "); // Display error message
+	        printf("Enter 1st Number Again => "); // Prompt user to enter the number again
+	    }
+	    
+	}while(n1 % 2 == 0); // Repeat the loop until an odd number is entered
+	
+	printf("Enter 2nd Odd Number => "); // Prompt user to enter second odd number
+	
+	 do{
+	    scanf("%d", &n2); // Take input from user
+	    
+	     if(n2 % 2 == 0) // Check if the number is even
+	    {
+	        printf("Number 2 is Even !\n"); // Display error message
+	        printf("Enter Number 2 again => "); // Prompt user to enter the number again
+	    }
+	    
+	}while(n2 % 2 == 0); // Repeat the loop until an odd number is entered
+	
+	printf("\n\nSum of %d and %d is %d",n1,n2, n1+n2); // Display the sum of the two odd numbers entered by the user
+	
+	return 0;
+
+}
