4151e17b52f8943aacec09be4783e2bf6b162d20
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-06-11 01:21:58 +0500

    Add files via upload

---
+++ C Programming/Array/1D Array/Separate even and odd numbers in an array.c
@@ -0,0 +1,56 @@
+//Author: Uzair Ahmad
+
+#include <stdio.h>
+
+int main(){
+	
+	int size ;
+	
+	printf("Enter Size for Array : ");
+	scanf(" %d", &size) ;
+	
+	int number[size];
+	
+	// Getting Input from User ;
+	
+	printf("Enter %d Number of Your Wish : \n",size);
+	
+	for(int i = 0 ; i < size; i++)
+	{
+		scanf("%d" , &number[i]);
+	}
+	
+	// Printing the Number Which You Enter ;
+	
+	printf("You Enter the Following Number's => \n ");
+	
+	for(int i = 0 ; i < size; i++)
+	{
+		printf("%d ", number[i]);
+	}
+		
+	printf("\nEven Numbers => \n ");
+		
+	// Checking Even Numbers ;
+	for(int i = 0 ; i < size; i++)
+	{
+		if( number[i] % 2 == 0)
+		{
+			printf("%d ", number[i]);
+		}
+	}
+	
+	printf("\nOdd Numbers => \n");
+		
+	// Checking Odd Numbers ;
+	for(int i = 0 ; i < size; i++)
+	{
+		if( number[i] % 2 != 0)
+		{
+			printf("%d ", number[i]);
+		}
+	}
+	
+	return 0 ;
+
+}
