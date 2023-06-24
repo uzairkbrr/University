5f21067ae8e431b445532bc8b035cc007ec15b9a
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-06-25 02:27:14 +0500

    Add files via upload

---
+++ C Programming/String/String Copy and Character Count.c
@@ -0,0 +1,31 @@
+//Author: Uzair Ahmad
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void main()
+{
+	char string1[100], string2[100];
+
+    printf("Input the string : ");
+	fgets(string1, sizeof string1, stdin);
+	
+	int i=0;
+	
+	// Copying characters from string1 to string2
+	while(string1[i]!='\0')
+	{
+	    string2[i] = string1[i];
+	    i++;
+	}
+	
+	// Makes sure that the string is NULL terminated
+	string2[i] = '\0';
+	
+	printf("\nThe First string is : %s\n", string1);
+	printf("The Second string is : %s\n", string2);
+	printf("Number of characters copied : %d\n\n", i);
+
+}
