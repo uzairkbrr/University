09e654cc13b36b9050b31683c30294c56629f4cc
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-06-30 15:46:59 +0500

    Add files via upload

---
+++ C Programming/String/Word Count in a String.c
@@ -0,0 +1,42 @@
+//Author: Uzair Ahmad
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+void main()
+{
+	int str_size ;
+
+    
+    printf("------------------------------------------------------\n");
+	printf("Enter Size for String : ");
+	scanf("%d", &str_size);
+			    	
+  	char str[str_size];	
+    
+	printf("Input the string : ");
+    fgets(str, sizeof str, stdin);	
+	
+   
+    int word = 1;
+
+    int  i = 0;
+    while(str[i]!='\0')
+    {
+      
+        if(str[i]==' ' || str[i]=='\t')
+        {
+            word++;
+        }
+
+        i++;
+    }
+
+    printf("Total number of words in the string is : %d\n", word);
+    
+    printf("------------------------------------------------------\n");
+
+}
