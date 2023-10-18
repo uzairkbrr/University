9ca471a853ea9c7dafa9e2ee26e15bde32f2c3b1
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-10-18 22:02:33 +0500

    Add files via upload

---
+++ JavaScript/Variables.js
@@ -0,0 +1,6 @@
+const accountId = 85;
+let accountEmail = "uzairdev8@gmail.com";
+var accountPassword = "12345";
+let accountCity = "Islamabad";
+
+console.table(accountId, accountEmail, accountPassword, accountCity);
