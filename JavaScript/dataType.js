365d140b3bd51c8e8f80bd40aa3aa8b7b5e3eb10
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-10-20 18:46:58 +0500

    Add files via upload

---
+++ JavaScript/dataType.js
@@ -0,0 +1,13 @@
+"use strict"; // use our code as newer version
+
+// alert("Hello, There") // I'm using nodejs, not browser
+
+// number => 2 to power 52
+// BigInt
+// String => "";
+// Boolean => true / false
+// null => standalone value
+// undefined =>
+// Symbol => unique
+
+// Object
