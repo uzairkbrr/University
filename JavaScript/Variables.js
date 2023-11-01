d91563422fc52cf26e51272da3fb3e56e54b6e1b
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-11-01 22:39:47 +0500

    Delete JavaScript/Variables.js

--- JavaScript/Variables.js
+++
@@ -1,6 +0,0 @@
-const accountId = 85;
-let accountEmail = "uzairdev8@gmail.com";
-var accountPassword = "12345";
-let accountCity = "Islamabad";
-
-console.table(accountId, accountEmail, accountPassword, accountCity);
