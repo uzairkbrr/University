5d59db578b466fe7dc48a4bde32d7c598e3c5f3a
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-12-21 00:38:26 +0500

    Add files via upload

---
+++ JavaScript/03-Basics/07_HowJsWork.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// what is the value of "this" in web ?
+// Answer: window
+
+// value of "this" for global scope is different in window & nodejs (compiler)
+
+// Types of Execution context
+// 1. Global EC, 2. Fucntional EC, 3. eval EC
