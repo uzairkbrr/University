0a22b69e9ccb9940cceb751dd3090d91e6859490
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2023-12-28 20:32:22 +0500

    Add files via upload

---
+++ JavaScript/04-Control Flow/one.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// if statement
+
+// if (2 !== "2") {
+//   console.log("executed");
+// }
+
+// Operator used in conditional Statement
+
+// < , > , <= , >= , == , != , === , !===
+
+// let temprature = 41;
+
+// if (temprature < 50) {
+//   console.log("Temprature is less than 50");
+// } else {
+//   console.log("Temprature is greater than 50");
+// }
+
+// let balance = 58;
+
+// if (balance < 20) {
+//   console.log("Balance is Less than 2");
+// } else if (balance < 40) {
+//   console.log("Balance is Less than 40");
+// } else if (balance < 60) {
+//   console.log("Balance is Less than 60");
+// } else {
+//   console.log("Balance is Less than 80");
+// }
+
+// let isLoggedIn = true;
+// let haveCard = true;
+
+// if (isLoggedIn && haveCard) {
+//   console.log("You can buy anything from our Website");
+// }
+
+let isLoggedInFromGoogle = false;
+let isLoggedInFromEmail = true;
+
+if (isLoggedInFromEmail || isLoggedInFromGoogle) {
+  console.log("User Logged In");
+}
