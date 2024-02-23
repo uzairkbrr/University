63cf64b4e5143b008cda6a8c802bc74a3712e028
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2024-02-24 01:09:08 +0500

    Add files via upload

---
+++ Assembly Language/e02-02.asm
@@ -0,0 +1,18 @@
+; a program to add three numbers accessed using a single label
+[org 0x0100]
+
+mov ax, [num1] 
+mov bx, [num1+2] 
+
+add ax, bx
+mov bx, [num1+4] 
+
+add ax, bx 
+mov [num1+6], ax 
+
+mov ax, 0x4c00
+int 0x21
+
+num1: dw 5
+      dw 10
+      dw 15
