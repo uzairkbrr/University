7db89a7de449fa5aeaf72271a4a1f69026dc8528
Uzair Ahmad <112511661+Uzairahmad8@users.noreply.github.com>
2024-04-23 20:01:22 +0500

    Add files via upload

---
+++ JavaScript/Passanger Counter App/3) Reassigning and Incrementing.js
@@ -0,0 +1,31 @@
+// document.getElementById("count").innerText = 5
+
+// let count = 5
+
+// console.log(count)
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+
+let bonusPoints = 50;
+
+// for(let i=1; i<=50; i++){
+//     bonusPoints += 1;
+// }
+
+bonusPoints = bonusPoints + 50;
+console.log("Bonus Points: " + bonusPoints);
+
+// for(let i = 100; i > 25; i--){
+//     bonusPoints -= 1;
+// }
+
+bonusPoints = bonusPoints - 75;
+console.log("Bonus Points: " + bonusPoints);
+
+// for(let i = 25; i < 70; i++){
+//     bonusPoints += 1;
+// }
+
+bonusPoints = bonusPoints + 45;
+console.log("Bonus Points: " + bonusPoints);
