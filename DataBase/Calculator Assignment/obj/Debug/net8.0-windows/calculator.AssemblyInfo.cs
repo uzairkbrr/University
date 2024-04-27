80d9bcf98210a82cbd3b57813302b06d72c68fec
Uzair Ahmad <uzairdev8@gmail.com>
2024-04-27 21:36:42 +0500

    .

--- DataBase/Calculator Assignment/obj/Debug/net8.0-windows/calculator.AssemblyInfo.cs
+++ DataBase/Calculator Assignment/obj/Debug/net8.0-windows/calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44fee879ba1d7a48522c6e5fe72d2cffb74843db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2befc62a4d8d1b70fbf18c61629a086143cdd48")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
