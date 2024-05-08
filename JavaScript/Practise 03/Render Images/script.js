0d20c84dbfed2596ea22a7e69c17847a501d0bf0
Uzair Ahmad <uzairdev8@gmail.com>
2024-05-08 15:13:50 +0500

    .

---
+++ JavaScript/Practise 03/Render Images/script.js
@@ -0,0 +1,22 @@
+// Create a function that renders the three team images
+// Use a for loop, template strings (``), plus equals (+=)
+// .innerHTML to solve the challenge.
+
+const imgs = [
+    "images/hip1.jpg",
+    "images/hip2.jpg",
+    "images/hip3.jpg"
+]
+
+
+const container = document.getElementById("container")
+
+
+function renderImages() {
+    let imgsString = "";
+    for ( let i = 0; i < imgs.length; i++ ) {
+        imgsString += `<img class="team-img" src="${imgs[i]}"`
+    }
+
+    container.innerHTML = imgsString
+}
---
+++ JavaScript/Practise 03/sentence generator.js
@@ -0,0 +1,28 @@
+// The generateSentence(desc, arr) takes two parameterer: a description and an array.
+// It should return a string based upon the description and array.
+
+// Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
+// it should return the string: "The 3 largest countries are China, India, USA"
+
+// Example 2:If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
+// "The 2 best fruits are Apples, Bananas"
+
+// Use both a for loop and a template string to solve the challenge
+function generateSentence(desc, arr) {
+    let sentence = `The ${arr.length} ${desc} is `;
+    
+    for(let i=0; i<arr.length; i++) {
+        sentence += ` ${arr[i]}`;
+        
+           if (i !== arr.length - 1) {
+            sentence += ",";
+        }
+    }
+    
+    console.log( sentence );
+}
+
+
+
+generateSentence("best fruits", ["Apples", "Bananas"]);
+generateSentence("largest countries", ["China", "India", "USA"]);
