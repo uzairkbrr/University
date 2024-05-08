2a11cdb0e4aed7d023c07d329d967daea1f8ff82
Uzair Ahmad <uzairdev8@gmail.com>
2024-05-08 14:55:58 +0500

    .

--- JavaScript/Practise 03/addEventListener and object in array/script.js
+++ JavaScript/Practise 03/addEventListener and object in array/script.js
@@ -12,3 +12,23 @@ let data = [
 // Fetch the button from the DOM, store it in a variable
 // Use addEventListener() to listen for button clicks
 // Log Jane's score when the button is clicked (via data)
+
+const btn = document.getElementById("btn")
+const clearBtn = document.getElementById("clear-btn")
+
+btn.addEventListener("click", function() {
+    let p = document.createElement("p")
+    let text = document.createTextNode(`Player: ${data[0].player}, Score: ${data[0].score} `)
+
+    p.appendChild(text)
+    document.body.appendChild(p)
+
+} )
+
+
+clearBtn.addEventListener("click", function() {
+    let logParagraphs = document.querySelectorAll("p");
+    logParagraphs.forEach(paragraph => {
+        paragraph.remove();
+    });
+})
