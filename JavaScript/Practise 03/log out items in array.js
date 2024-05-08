cbe2f346e516b33a575c2319f64a4f1b5d2985b0
Uzair Ahmad <uzairdev8@gmail.com>
2024-05-08 14:32:41 +0500

    .

---
+++ JavaScript/Practise 03/log out items in array.js
@@ -0,0 +1,14 @@
+let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
+
+// Create a function that takes a single parameter, an array,
+// and logs all the items of the array to the console.
+// Call the function while passing in myCourses as an argument
+
+function logoutArray( array ) {
+    for( let i = 0; i < array.length; i++) {
+        console.log(array[i])
+    }
+}
+
+
+logoutArray(myCourses)
---
+++ JavaScript/Practise 03/save to localStorage.js
@@ -0,0 +1,12 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+
+// storing into localStorage
+// localStorage.setItem("key", "85");
+
+// Stored into localStorage and then Comment out the code for storing to check whether the value is stored in or not.
+let key = localStorage.getItem("key");
+
+console.log(key);
