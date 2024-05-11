28eba8303811e285c0278017523b0e759cdd7e94
Uzair Ahmad <uzairdev8@gmail.com>
2024-05-11 19:39:33 +0500

    .

---
+++ JavaScript/JavaScript.Info/JavaScript Fundamentals/2.10) Conditional branching/rewrite if into ternary opeator 2.js
@@ -0,0 +1,27 @@
+// Rewrite if..else using multiple ternary operators '?'.
+
+// For readability, it’s recommended to split the code into multiple lines.
+
+
+let message;
+
+let login = prompt("What is your Designation? ");
+
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+
+let message2 = (login == "Employee") ? "Hello" : 
+              (login == "Director") ? "Greetings" :
+              (login == "") ? "No Login" : "" 
+
+
+
+
