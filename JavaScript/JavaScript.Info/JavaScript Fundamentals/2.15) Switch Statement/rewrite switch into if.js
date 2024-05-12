2a18db7bad3eedf0c5dd84577b88a971d08f5791
Uzair Ahmad <uzairdev8@gmail.com>
2024-05-12 20:54:10 +0500

    .

---
+++ JavaScript/JavaScript.Info/JavaScript Fundamentals/2.15) Switch Statement/rewrite switch into if.js
@@ -0,0 +1,24 @@
+switch (browser) {
+    case 'Edge':
+      alert( "You've got the Edge!" );
+      break;
+  
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera':
+      alert( 'Okay we support these browsers too' );
+      break;
+  
+    default:
+      alert( 'We hope that this page looks ok!' );
+  }
+
+
+if ( browser === "Edge" ) {
+   console.log("You've got the Edge!");
+} else if ( browser === "Chrome" || browser === "Firefox" || browser === "Safari" || browser ===  "Opera") {
+    console.log("Okay we support these browers too.");
+} else {
+    console.log("We hope that this page looks ok!");
+}
